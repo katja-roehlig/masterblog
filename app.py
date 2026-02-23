0f807aa089a92c19fcedf915ec4fdd99631e025e
Katja Röhlig <katja-roehlig@mail.de>
2026-02-23 20:12:09 +0100

    create add route

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 import json
+import uuid
+import random
 
 app = Flask(__name__)
 # posts = [
@@ -22,11 +24,28 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
     with open("blog_storage.json", "r") as file:
-        blog_list = json.loads(file.read())
-        print(blog_list)
+        blog_list = json.load(file)
         return render_template("index.html", blogs=blog_list)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_id = random.randint(1, 1000000)
+        title = request.form.get("title")
+        author = request.form.get("author")
+        content = request.form.get("content")
+        new_blog = {"id": new_id, "title": title, "author": author, "content": content}
+        with open("blog_storage.json", "r") as file:
+            blog_list = json.load(file)
+        blog_list.append(new_blog)
+        with open("blog_storage.json", "w") as newfile:
+            json.dump(blog_list, newfile)
+        return redirect(url_for("index"))
+    else:
+        return render_template("add.html")
 
 
 if __name__ == "__main__":
