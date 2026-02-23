19b02d6b584c63c97742f65dc87423187f12c394
Katja Röhlig <katja-roehlig@mail.de>
2026-02-23 18:50:30 +0100

    create first route

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+# posts = [
+#     {
+#         "id": 1,
+#         "author": "John Doe",
+#         "title": "First Post",
+#         "content": "This is my first post.",
+#     },
+#     {
+#         "id": 2,
+#         "author": "Jane Doe",
+#         "title": "Second Post",
+#         "content": "This is another post.",
+#     },
+# ]
+# with open("blog_storage.json", "w") as newfile:
+#     json_string = json.dumps(posts)
+#     newfile.write(json_string)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    with open("blog_storage.json", "r") as file:
+        blog_list = json.loads(file.read())
+        print(blog_list)
+        return render_template("index.html", blogs=blog_list)
 
 
 if __name__ == "__main__":
